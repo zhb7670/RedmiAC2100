0d1338e2fb3d5c8ff36f69064cccc3ff3c2f6555
zhb7670 <52443818+zhb7670@users.noreply.github.com>
2021-10-19 08:06:18 +0530

    Initial commit

---
+++ diy-part1.sh
@@ -0,0 +1,3 @@
+#Add Custom Feeds
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
---
+++ diy-part2.sh
@@ -0,0 +1,3 @@
+#There is nothing.
+#You can view others files in this repository.
+#Good luck to you.
