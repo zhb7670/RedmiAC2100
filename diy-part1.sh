fc4d1b991aff476b4abc3fdf29f9e1c395b0d332
zhb7670 <52443818+zhb7670@users.noreply.github.com>
2021-10-19 23:24:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,9 +1,4 @@
 #Add Custom Feeds
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-echo 'src-git routing https://git.openwrt.org/feed/routing' >>feeds.conf.default
-echo 'src-git telephony https://git.openwrt.org/feed/telephony' >>feeds.conf.default
+
